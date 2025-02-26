cba5368a4bf9e6159b304f5dc750c007206eaff9
Jérôme Commaret <jcommaret@gmail.com>
2025-02-26 19:45:55 +0100

    Configuration et préparation du projet pour le déploiement
    - Ajout de la configuration EAS pour le build et la soumission
    - Mise à jour de app.json avec les identifiants iOS et Android
    - Suppression du fichier App.tsx initial
    - Mise à jour de l'import de l'App dans index.ts
    - Ajout de react-native-dotenv aux dépendances

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import styles from "./styles/styles";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
+    </View>
+  );
+}
